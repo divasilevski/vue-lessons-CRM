9482be2d9ba8c4a19c83e58db8235fd836489af6
divasilevski <divasilevski@gmail.com>
2020-02-20 21:27:23 +0300

    Day 1

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,70 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
+    meta: { layout: "main" },
     component: Home
+  },
+  {
+    path: "/login",
+    name: "Login",
+    meta: { layout: "empty" },
+    component: () => import("../views/Login.vue")
+  },
+  {
+    path: "/register",
+    name: "Register",
+    meta: { layout: "empty" },
+    component: () => import("../views/Register.vue")
+  },
+  {
+    path: "/categories",
+    name: "Categories",
+    meta: { layout: "main" },
+    component: () => import("../views/Categories.vue")
+  },
+  {
+    path: "/detail-record",
+    name: "DetailRecord",
+    meta: { layout: "main" },
+    component: () => import("../views/DetailRecord.vue")
+  },
+  {
+    path: "/history",
+    name: "History",
+    meta: { layout: "main" },
+    component: () => import("../views/History.vue")
+  },
+  {
+    path: "/planning",
+    name: "Planning",
+    meta: { layout: "main" },
+    component: () => import("../views/Planning.vue")
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    meta: { layout: "main" },
+    component: () => import("../views/Profile.vue")
+  },
+  {
+    path: "/record",
+    name: "Record",
+    meta: { layout: "main" },
+    component: () => import("../views/Record.vue")
   }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;

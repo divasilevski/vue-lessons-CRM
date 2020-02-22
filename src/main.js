6292c2d9a853acf13eab0717b64defde334f4032
divasilevski <divasilevski@gmail.com>
2020-02-22 15:02:51 +0300

    Day 6 (firebase)

--- src/main.js
+++ src/main.js
@@ -8,14 +8,34 @@ import messagePlugin from "./utils/message.plugin";
 import "./registerServiceWorker";
 import "materialize-css/dist/js/materialize.min";
 
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
 Vue.config.productionTip = false;
 
 Vue.use(messagePlugin);
 Vue.use(Vuelidate);
 Vue.filter("date", dateFilter);
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+firebase.initializeApp({
+  apiKey: "AIzaSyCocEmsXZ-6-ZoYSiCX7dNfyqfesZ08w1E",
+  authDomain: "di-vue-lessons.firebaseapp.com",
+  databaseURL: "https://di-vue-lessons.firebaseio.com",
+  projectId: "di-vue-lessons",
+  storageBucket: "di-vue-lessons.appspot.com",
+  messagingSenderId: "239497055590",
+  appId: "1:239497055590:web:215c60996e7431d2bb4ca7",
+  measurementId: "G-TCG342SVHD"
+});
+
+let app;
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount("#app");
+  }
+});
---
+++ src/store/auth.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+
+export default {
+  state: {},
+  mutations: {},
+  actions: {
+    async login({ dispatch, commit }, { email, password }) {
+      try {
+        await firebase.auth().signInWithEmailAndPassword(email, password);
+      } catch (error) {
+        throw error;
+      }
+    }
+  }
+};

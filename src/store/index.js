c6a009b44c246f15153807f945d6d8896a57f0cd
divasilevski <divasilevski@gmail.com>
2020-02-23 11:56:36 +0300

    Day 9 (API)

--- src/store/index.js
+++ src/store/index.js
@@ -17,11 +17,20 @@ export default new Vuex.Store({
       state.error = null;
     }
   },
-  actions: {},
+  actions: {
+    async fetchCurrency() {
+      const key = process.env.VUE_APP_FIXER;
+      const res = await fetch(
+        `http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,RUB`
+      );
+      return await res.json();
+    }
+  },
   getters: {
     error: state => state.error
   },
   modules: {
-    auth, info
+    auth,
+    info
   }
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <div class="page-title">
         <h3>Счет</h3>
 
-        <button class="btn waves-effect waves-light btn-small">
+        <button class="btn waves-effect waves-light btn-small" @click="refresh">
           <i class="material-icons">refresh</i>
         </button>
       </div>
@@ -12,9 +12,9 @@
       <Loader v-if="loading" />
 
       <div v-else class="row">
-        <HomeBill />
+        <HomeBill :rates="currency.rates" />
 
-        <HomeCurrency />
+        <HomeCurrency :rates="currency.rates" :date="currency.date" />
       </div>
     </div>
   </div>
@@ -29,10 +29,20 @@ export default {
     loading: true,
     currency: null
   }),
+  async mounted() {
+    this.currency = await this.$store.dispatch("fetchCurrency");
+    this.loading = false;
+  },
+  methods: {
+    async refresh() {
+      this.loading = true;
+      this.currency = await this.$store.dispatch("fetchCurrency");
+      this.loading = false;
+    }
+  },
   components: {
-    HomeBill, 
+    HomeBill,
     HomeCurrency
   }
-
 };
 </script>

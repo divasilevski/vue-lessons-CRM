b952befba43c3fd1912f23b5498487a75e996ffd
divasilevski <divasilevski@gmail.com>
2020-02-24 18:13:40 +0300

    Day 18 (chart)

--- src/views/History.vue
+++ src/views/History.vue
@@ -5,7 +5,7 @@
     </div>
 
     <div class="history-chart">
-      <canvas></canvas>
+      <canvas ref="canvas"></canvas>
     </div>
 
     <Loader v-if="loading" />
@@ -30,8 +30,10 @@
 <script>
 import paginationMixin from "../mixins/pagination.mixin";
 import HistoryTable from "../components/HistoryTable";
+import { Pie } from "vue-chartjs";
 export default {
   name: "History",
+  extends: Pie,
   mixins: [paginationMixin],
   data: () => ({
     records: [],
@@ -40,18 +42,49 @@ export default {
   async mounted() {
     this.records = await this.$store.dispatch("fetchRecords");
     const categories = await this.$store.dispatch("fetchCategories");
-    this.setupPagination(
-      this.records.map(record => {
-        return {
-          ...record,
-          categoryName: categories.find(c => c.id === record.categoryId).title,
-          typeClass: record.type === "income" ? "green" : "red",
-          typeText: record.type === "income" ? "Доход" : "Расход"
-        };
-      })
-    );
+
+    this.setup(categories);
 
     this.loading = false;
+  },
+  methods: {
+    setup(categories) {
+      this.setupPagination(
+        this.records.map(record => {
+          return {
+            ...record,
+            categoryName: categories.find(c => c.id === record.categoryId)
+              .title,
+            typeClass: record.type === "income" ? "green" : "red",
+            typeText: record.type === "income" ? "Доход" : "Расход"
+          };
+        })
+      );
+
+      this.renderChart({
+        labels: categories.map(c => c.title),
+        datasets: [
+          {
+            label: "Расходы по категориям",
+            data: categories.map(c => {
+              return this.records.reduce((total, r) => {
+                if (r.categoryId === c.id && r.type === "outcome") {
+                  total += +r.amount;
+                }
+                return total;
+              }, 0);
+            }),
+            backgroundColor: [
+              'rgba(255, 99, 100, 0.2)',
+              'rgba(25, 99, 255, 0.2)',
+              'rgba(25, 99, 100, 0.2)',
+              'rgba(255, 255, 100, 0.2)',
+              'rgba(0, 99, 0, 0.2)'
+            ]
+          }
+        ]
+      });
+    }
   },
   components: {
     HistoryTable
